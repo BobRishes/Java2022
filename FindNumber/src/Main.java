e5a4fbf57864e3d602ece5a3a89e1b2f3a0ec65b
BobRishes <111277512+BobRishes@users.noreply.github.com>
2022-09-23 17:19:38 +0300

    Add files via upload

---
+++ FindNumber/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+		int[] sayilar= new int[] {1,2,5,7,8,0};
+		
+		int aranacak=0;	
+		
+		for(int i=0;i<=sayilar.length;i++) {		
+			
+		if(aranacak==i) {
+			System.out.println("sayı dizide mevcut");
+		
+		}
+		
+		
+		}
+	}
+
+}
